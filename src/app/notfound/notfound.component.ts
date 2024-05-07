2cd936a45781f78b7487477e2231ea9322aafd91
Zrinyi Patrik <patrikzrinyi404@gmail.com>
2024-05-07 18:03:58 +0200

    Add lifecycle hook to 404 spinner

--- src/app/notfound/notfound.component.ts
+++ src/app/notfound/notfound.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-notfound',
   standalone: true,
   imports: [
-    MatProgressSpinner
+    MatProgressSpinner,
+    NgIf
   ],
   templateUrl: './notfound.component.html',
   styleUrl: './notfound.component.css'
 })
 export class NotfoundComponent {
+  showSpinner = false;
 
+  ngAfterContentInit(): void {
+    this.showSpinner = true;
+  }
 }
